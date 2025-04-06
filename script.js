500ca40dd0901a5e62edc56d39e5c8bf8f584f20
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-06 15:48:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let value=document.getElementById("status");
 button.addEventListener("click", function(){
 	let h1=document.createElement("h1");
 	h1.textContent="Entered Metaverse";
+	h1.id="status";
 	value.replaceWith(h1);
 	
 })
